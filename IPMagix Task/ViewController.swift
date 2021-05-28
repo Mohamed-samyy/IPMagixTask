2247046bc5924a95ff1e1b16cc2ba8acdb0eb987
Mohamed-Samyy <mohamedsamyy.ms@gmail.com>
2021-05-28 13:11:03 +0200

    Initial Commit

---
+++ IPMagix Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IPMagix Task
+//
+//  Created by Mohamed Samy on 5/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
